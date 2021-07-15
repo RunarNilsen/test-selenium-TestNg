4336bc92c07a565776b89614a9afb04a977f9c71
muhammet-cakmak <cakmakmuhammet12@gmail.com>
2021-07-15 22:46:54 +0200

    ExcelUtils  was added

---
+++ src/test/java/com/cybertek/tests/day16_ddf/ExcelUtilsDemo.java
@@ -0,0 +1,44 @@
+package com.cybertek.tests.day16_ddf;
+
+import com.cybertek.utilities.ExcelUtils;
+import org.testng.annotations.Test;
+
+import java.util.List;
+import java.util.Map;
+
+public class ExcelUtilsDemo {
+
+    @Test
+    public void readExcelFile(){
+
+        // Create ExcelUtils object from ExcelUtils.class
+        ExcelUtils qa3short = new ExcelUtils("src/test/resources/VytrackTestData.xlsx", "QA3-short");
+
+        // How many rows in the sheet
+        System.out.println("qa3short.rowCount() = " + qa3short.rowCount());
+
+        // How many columns in the sheet
+        System.out.println("qa3short.columnCount() = " + qa3short.columnCount());
+
+        // Get all column names. It returns list of strings
+        System.out.println("qa3short.getColumnsNames() = " + qa3short.getColumnsNames());
+
+        // Get all data in list of maps
+        // Ech row in the excel sheet refers to one map. map consists of key and value pairs
+        List<Map<String, String>> dataList = qa3short.getDataList();
+        for (Map<String, String> oneRow : dataList) {
+            System.out.println(oneRow);
+        }
+
+        // get "Nona" as a value
+        System.out.println("dataList.get(2) = " + dataList.get(2)); // it return the 3rd row as a map
+        System.out.println("firstname = " + dataList.get(2).get("firstname"));
+
+
+    }
+
+
+
+
+
+}
